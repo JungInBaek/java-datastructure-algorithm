e0b7b0a3b111e5c2c4eae728a5b6de82d7c1f513
JungInbaek <bji1527@naver.com>
2022-11-11 23:52:14 +0900

    객체 배열 이진 검색 학습
    자연 정렬된 배열,
    자연 정렬되지 않은 배열에서 이진 검색

---
+++ src/chap03/Q6.java
@@ -0,0 +1,37 @@
+package chap03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("요솟수: ");
+        int num = scanner.nextInt();
+        int[] x = new int[num];
+
+        System.out.println("오름차순으로 입력하세요.");
+
+        System.out.print("x[0]: ");
+        x[0] = scanner.nextInt();
+
+        for (int i = 1; i < num; i++) {
+            do {
+                System.out.print("x[" + i + "]: ");
+                x[i] = scanner.nextInt();
+            } while (x[i] < x[i - 1]);
+        }
+
+        System.out.print("검색할 값: ");
+        int key = scanner.nextInt();
+
+        int idx = Arrays.binarySearch(x, key);
+
+        if (idx < 0) {
+            System.out.println("삽입 포인트는 " + -(idx + 1) + "입니다.");
+        } else {
+            System.out.println("그 값은 x[" + idx + "]에 있습니다.");
+        }
+    }
+}
