a310e11674475ec965719581b716bb4026e46744
JungInbaek <bji1527@naver.com>
2022-11-13 00:45:43 +0900

    연습 문제 Q3 마무리

--- src/chap04/Q3.java
+++ src/chap04/Q3.java
@@ -4,7 +4,8 @@ public class Q3 {
     // int형 고정 길이 스택
     private int[] stk;      // 스택용 배열
     private int capacity;   // 스택 용량
-    private int ptr;        // 스택 포인터
+    private int ptrA;        // 스택 포인터
+    private int ptrB;
 
     // 실행 시 예외: 스택이 비어 있음
     public class EmptyIntStackException extends RuntimeException {
@@ -18,8 +19,9 @@ public class Q3 {
 
     // 생성자
     public Q3(int maxlen) {
-        ptr = 0;
         capacity = maxlen;
+        ptrA = 0;
+        ptrB = capacity - 1;
         try {
             stk = new int[capacity];        // 스택 본체용 배열을 생성
         } catch (OutOfMemoryError e) {      // 생성할 수 없음
@@ -27,38 +29,77 @@ public class Q3 {
         }
     }
 
-    // 스택에 x를 푸시
-    public int push(int x) throws OverflowIntStackException {
-        if (ptr >= capacity) {
+    // 스택A에 x를 푸시
+    public int pushA(int x) throws OverflowIntStackException {
+        if (ptrA >= ptrB + 1) {
             throw new OverflowIntStackException();
         }
-        return stk[ptr++] = x;
+        return stk[ptrA++] = x;
     }
 
-    // 스택에서 데이터를 팝(꼭대기에 있는 데이터를 꺼냄)
-    public int pop() throws EmptyIntStackException {
-        if (ptr <= 0) {
+    // 스택B에 x를 푸시
+    public int pushB(int x) throws OverflowIntStackException {
+        if (ptrB <= ptrA - 1) {
+            throw new OverflowIntStackException();
+        }
+        return stk[ptrB--] = x;
+    }
+
+    // 스택A에서 데이터를 팝(꼭대기에 있는 데이터를 꺼냄)
+    public int popA() throws EmptyIntStackException {
+        if (ptrA <= 0) {
             throw new EmptyIntStackException();
         }
-        return stk[--ptr];
+        return stk[--ptrA];
     }
 
-    // 스택에서 데이터를 피크(꼭대기에 있는 데이터를 들여다봄)
-    public int peek() throws EmptyIntStackException {
-        if (ptr <= 0) {
+    // 스택B에서 데이터를 팝(꼭대기에 있는 데이터를 꺼냄)
+    public int popB() throws EmptyIntStackException {
+        if (ptrB >= capacity) {
             throw new EmptyIntStackException();
         }
-        return stk[ptr - 1];
+        return stk[++ptrB];
     }
 
-    // 스택을 비움
-    public void clear() {
-        ptr = 0;
+    // 스택A에서 데이터를 피크(꼭대기에 있는 데이터를 들여다봄)
+    public int peekA() throws EmptyIntStackException {
+        if (ptrA <= 0) {
+            throw new EmptyIntStackException();
+        }
+        return stk[ptrA - 1];
     }
 
-    // 스택에서 x를 찾아 인덱스(없으면 -1)를 반환
-    public int indexOf(int x) {
-        for (int i = ptr - 1; i >= 0; i--) {
+    // 스택B에서 데이터를 피크(꼭대기에 있는 데이터를 들여다봄)
+    public int peekB() throws EmptyIntStackException {
+        if (ptrB >= capacity) {
+            throw new EmptyIntStackException();
+        }
+        return stk[ptrB + 1];
+    }
+
+    // 스택A를 비움
+    public void clearA() {
+        ptrA = 0;
+    }
+
+    // 스택B를 비움
+    public void clearB() {
+        ptrB = capacity - 1;
+    }
+
+    // 스택A에서 x를 찾아 인덱스(없으면 -1)를 반환
+    public int indexOfA(int x) {
+        for (int i = ptrA - 1; i >= 0; i--) {
+            if (stk[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 스택B에서 x를 찾아 인덱스(없으면 -1)를 반환
+    public int indexOfB(int x) {
+        for (int i = ptrB + 1; i <= capacity - 1; i++) {
             if (stk[i] == x) {
                 return i;
             }
@@ -72,26 +113,49 @@ public class Q3 {
     }
 
     // 스택에 쌓여 있는 데이터 개수를 반환
-    public int size() {
-        return ptr;
+    public int sizeA() {
+        return ptrA;
+    }
+
+    public int sizeB() {
+        return capacity - (ptrB + 1);
     }
 
     // 스택이 비어 있는가?
-    public boolean isEmpty() {
-        return ptr <= 0;
+    public boolean isEmptyA() {
+        return ptrA <= 0;
+    }
+
+    public boolean isEmptyB() {
+        return ptrB >= capacity - 1;
     }
 
     // 스택이 가득 찾는가?
-    public boolean isFull() {
-        return ptr >= capacity;
+    public boolean isFullA() {
+        return ptrA >= ptrB + 1;
+    }
+
+    public boolean isFullB() {
+        return ptrB <= ptrA - 1;
     }
 
     // 스택 안의 모든 데이터를 바닥 -> 꼭대기 순서로 출력
-    public void dump() {
-        if (ptr <= 0) {
+    public void dumpA() {
+        if (ptrA <= 0) {
             System.out.println("스택이 비어 있습니다.");
         } else {
-            for (int i = 0; i < ptr; i++) {
+            for (int i = 0; i < ptrA; i++) {
+                System.out.print(stk[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void dumpB() {
+        if (ptrB <= capacity - 1) {
+            System.out.println("스택이 비어 있습니다.");
+        } else {
+            for (int i = capacity - 1; i < ptrB; i--) {
                 System.out.print(stk[i] + " ");
             }
             System.out.println();
