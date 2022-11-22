5c5e2fc3c97e9149986ae1f7b0cedcff99ac32dd
JungInbaek <bji1527@naver.com>
2022-11-22 23:37:34 +0900

    재귀 알고리즘 기본 학습, 연습 문제 풀기
    재귀 호출로 팩토리얼, 최대공약수 구하기
    연습 문제 : Q1, Q2

---
+++ src/chap05/Q2.java
@@ -0,0 +1,29 @@
+package chap05;
+
+import java.util.Scanner;
+
+public class Q2 {
+    static int gcd(int x , int y) {
+        while(true) {
+            int n = x % y;
+            if (n == 0) {
+                return y;
+            }
+            x = y;
+            y = n;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("두 정수의 최대공약수를 구합니다.");
+
+        System.out.print("정수를 입력하세요: ");
+        int x = scanner.nextInt();
+        System.out.print("정수를 입력하세요: ");
+        int y = scanner.nextInt();
+
+        System.out.println("최대공약수는 " + gcd(x, y) + "입니다.");
+    }
+}
