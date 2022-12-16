2e30d9b33044d79e712282d02cd330445036b8a3
JungInbaek <bji1527@naver.com>
2022-12-16 23:02:04 +0900

    단순 선택 정렬 학습

--- src/chap06/SelectionSort.java
+++ src/chap06/SelectionSort.java
@@ -2,7 +2,7 @@ package chap06;
 
 import java.util.Scanner;
 
-public class BubbleSort3 {
+public class SelectionSort {
 
     // a[idx1]과 a[idx2]의 값을 교환
     static void swap(int[] a, int idx1, int idx2) {
@@ -11,25 +11,23 @@ public class BubbleSort3 {
         a[idx2] = t;
     }
 
-    // 버블 정렬 (버전 3)
-    static void bubbleSort(int[] a, int n) {
-        int k = 0;                              // a[k]보다 앞 쪽은 정렬을 마친 상태
-        while (k < n - 1) {
-            int last = n - 1;                   // 마지막으로 요소를 교환한 위치
-            for (int j = n - 1; j > k; j++) {   // 패스
-                if (a[j - 1] > a[j]) {
-                    swap(a, j - 1, j);
-                    last = j;                   // 마지막으로 요소를 교환한 위치
+    // 단순 선택 정렬
+    static void selectionSort(int[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;        // 아직 정렬되지 않은 부분에서 가장 작은 요소의 인덱스를 저장
+            for (int j = i + 1; j < n; j++) {
+                if (a[min] > a[j]) {
+                    min = j;
                 }
             }
-            k = last;
+            swap(a, i, min);    // 아직 정렬되지 않은 부분의 첫 요소와 가장 작은 요소를 교환
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("버블 정렬(버전 1)");
+        System.out.println("단순 선택 정렬");
         System.out.print("요솟수: ");
         int n = scanner.nextInt();
         int[] x = new int[n];
@@ -39,7 +37,7 @@ public class BubbleSort3 {
             x[i] = scanner.nextInt();
         }
 
-        bubbleSort(x, n);       // 배열 x를 버블 정렬
+        selectionSort(x, n);       // 배열 x를 단순 선택 정렬
 
         System.out.println("오름차순으로 정렬했습니다.");
         for (int i = 0; i < n; i++) {
